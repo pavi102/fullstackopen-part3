9ac7e762ac944eb097671f3ba33f324159894aa4
pavi102 <pavi1022@gmail.com>
2022-07-02 10:03:21 -0700

    configured express to serve static files

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 morgan.token("req-body", req => JSON.stringify(req.body));
 
 app.use(express.json());
+app.use(express.static("build"));
+
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :req-body")
 );

ea6102c8c46bd17c0c25cbf512c7ad801fb11370
pavi102 <pavi1022@gmail.com>
2022-07-03 15:01:22 -0700

    completed exercises 3.13-3.14

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
+
 const app = express();
 
 morgan.token("req-body", req => JSON.stringify(req.body));
@@ -13,68 +16,50 @@ app.use(
 );
 app.use(cors());
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-const generateId = () => {
-  return Math.floor(Math.random() * 99999);
-};
-
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+app.get("/api/persons", async (req, res) => {
+  const persons = await Person.find({});
+  return res.json(persons);
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const person = persons.find(person => person.id === Number(req.params.id));
-  console.log(person);
-  if (!person) {
-    return res.status(404).json({ error: `person not found` });
+app.get("/api/persons/:id", async (req, res) => {
+  try {
+    const person = await Person.findById(req.params.id);
+    if (!person) {
+      return res.status(404).json({ error: `person not found` });
+    }
+    return res.json(person);
+  } catch (err) {
+    res.status(400).send({ error: "malformatted id" });
   }
-  return res.json(person);
 });
 
-app.get("/info", (req, res) => {
+app.get("/info", async (req, res) => {
+  const personsLen = await (await Person.find({})).length;
   const date = new Date();
   res.send(`
-  <p>Phonebook has info for ${persons.length} people</p>
+  <p>Phonebook has info for ${personsLen} people</p>
   <p>${date}</p>
   `);
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", async (req, res) => {
   const { name, number } = req.body;
-  const existingPerson = persons.find(person => person.name === name);
+  // const existingPerson = await Person.findOne({ name: name });
 
   if (!name || !number) {
     return res.status(400).json({ error: "All fields must be filled" });
   }
-  if (existingPerson) {
-    return res.status(400).json({ error: "Name must be unique" });
-  }
+  // if (existingPerson) {
+  //   return res.status(400).json({ error: "Name must be unique" });
+  // }
 
-  const newPerson = { id: generateId(), name, number };
-  persons = persons.concat(newPerson);
-  return res.status(201).json(newPerson);
+  const person = new Person({
+    name,
+    number,
+  });
+
+  const savedPerson = await person.save();
+  return res.status(201).json(savedPerson);
 });
 
 const PORT = process.env.PORT || 3001;
